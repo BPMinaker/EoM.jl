0683b62ba9398f8cbdea8a24aa8bcf247ed7e6b4
BPMinaker <bminaker@gmail.com>
2017-04-01 13:09:11 -0400

    tex stuff

---
+++ src/tex_bode_pgfplot.jl
@@ -0,0 +1,55 @@
+function tex_bode_pgfplot(ins,outs)
+## Copyright (C) 2017, Bruce Minaker
+## tex_bode_pgfplot.jl is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2, or (at your option)
+## any later version.
+##
+## tex_bode_pgfplot.jl is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details at www.gnu.org/copyleft/gpl.html.
+##
+##--------------------------------------------------------------------
+
+nin=length(ins)
+nout=length(outs)
+s="\\section{Frequency Response Plots}\n"
+
+for i=1:nin  ## For each input
+	s="\\begin{figure}[hbtp]\n"
+	s*="\\begin{center}\n"
+	s*="\\begin{footnotesize}\n"
+	s*="\\begin{tikzpicture}\n"
+  s*="\\begin{semilogxaxis}[height=2in,width=5in,tick style={thin,black},"
+  s*="extra y ticks={0},extra y tick style={grid=major},major y grid style={dotted,black},"
+  s*="xlabel={Frequency [Hz]},ylabel={Transfer Function [dB]},enlarge x limits=false,"
+  s*="legend style={at={(1.0,1.03)},anchor=south east},legend columns=1,legend cell align=left,cycle list name=linestyles*]\n"
+	for j=1:nout  ## For each output
+		s*="\\addplot+[black,line width=1pt,mark=none] table[x=frequency,y=m$((i-1)*nout+j)]{bode.out};\n"
+		s*="\\addlegendentry{$(outs[j]) / $(ins[i])}\n"
+	end
+	s*="\\end{semilogxaxis}\n"
+	s*="\\end{tikzpicture}\n"
+	s*="\\begin{tikzpicture}\n"
+	s*="\\begin{semilogxaxis}[height=2in,width=5in,ymin=-180,ymax=180,ytick={-180,-90,0,90,180},"
+	s*="tick style={thin,black},extra y ticks={0},extra y tick style={grid=major},major y grid style={dotted,black},"
+	s*="xlabel={Frequency [Hz]},ylabel={Phase Angle [\$^\\circ\$]},enlargelimits=false,cycle list name=linestyles*,"
+	s*="restrict y to domain= -180:180,unbounded coords=jump]\n"
+	for j=1:nout
+		s*="\\addplot+[black,line width=1pt,mark=none] table[x=frequency,y=p$((i-1)*nout+j)]{bode.out};\n"
+	end
+	s*="\\end{semilogxaxis}\n"
+	s*="\\end{tikzpicture}\n"
+	s*="\\end{footnotesize}\n"
+	s*="\\caption{Frequency response: $(ins[i])}\n"
+	s*="\\label{bode_plot_$i}\n"
+	s*="\\end{center}\n"
+	s*="\\end{figure}\n\n"
+
+end
+s*="\\clearpage\n\n"
+
+s
+
+end ## Leave
---
+++ src/tex_eig_pgfplot.jl
@@ -0,0 +1,36 @@
+function tex_eig_pgfplot()
+## Copyright (C) 2017, Bruce Minaker
+## tex_eig_pgfplot.jl is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2, or (at your option)
+## any later version.
+##
+## tex_eig_pgfplot.jl is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details at www.gnu.org/copyleft/gpl.html.
+##
+##--------------------------------------------------------------------
+
+## Write the tex necessary to include the plots
+s="\\section{Eigenvalue Plot}\n"
+s*="\\begin{figure}[htbp]\n"
+s*="\\begin{center}\n"
+s*="\\begin{tikzpicture}\n"
+s*="\\begin{axis}[height=3in,width=5in,xmin=0,restrict y to domain=-100:100,restrict expr to domain={abs(y)}{0.001:100},xlabel={Speed [m/s]},ylabel={Eigenvalue [1/s]},tick style={thin,black},extra y ticks={0},extra y tick style={grid=major},major y grid style={dotted,black},enlarge x limits=false,legend style={at={(1.0,1.03)},anchor=south east},legend columns=-1]\n"
+s*="\\addplot+[only marks,mark=*,mark options={black,scale=0.6,line width=1pt}] table[x=speed,y=real]{eigen.out};\n"
+s*="\\addplot+[only marks,mark=*,mark options={black,fill=white,scale=1,line width=0.5pt}] table[x=speed,y expr=abs(\\thisrow{imag})]{eigen.out};\n"
+s*="\\legend{Real, Imaginary}\n"
+s*="\\end{axis}\n"
+s*="\\end{tikzpicture}\n"
+s*="\\caption[Eigenvalues vs speed]{Eigenvalues vs speed.  Note that only the positive values of the imaginary roots are plotted for clarity.}\n"
+s*="\\label{eigen_plot}\n"
+s*="\\end{center}\n"
+s*="\\end{figure}\n"
+s*="\\clearpage\n\n"
+
+s
+
+end ## Leave
+
+##y filter/.code={\\pgfmathparse{(2*(#1>0)-1)*max(abs(#1),0.001)/(abs(#1)>0.001)}\\pgfmathresult}
