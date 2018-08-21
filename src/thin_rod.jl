f19004512125e49fa6ef46b2824c6b71bdc0dac3
BPMinaker <bminaker@uwindsor.ca>
2018-08-21 00:51:03 -0400

    v0.7

--- src/thin_rod.jl
+++ src/thin_rod.jl
@@ -16,12 +16,12 @@ function thin_rod(name,ends,mass)
 ## thin_rod finds the mass matrix and mass centre of a thin rod, given
 ## end locations
 
-len_vec=diff(ends,2)
+len_vec=diff(ends,dims=2)
 i_mtx=-skew(len_vec)^2*mass/12
 
 item=body(name)
 item.mass=mass
-item.location=mean(ends,2)[:,1]
+item.location=0.5*(ends[:,1]+ends[:,2])
 item.moments_of_inertia=diag(i_mtx)
 item.products_of_inertia=-[i_mtx[1,2],i_mtx[2,3],i_mtx[3,1]]  ## Change sign; using defn of Ixy as +ve integral
 
