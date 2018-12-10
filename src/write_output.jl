54b80678d4e070f4edc0795e5c6b2fd0519cd676
BPMinaker <bminaker@uwindsor.ca>
2018-12-10 17:15:04 -0500

    gnuplot stuff

--- src/write_output.jl
+++ src/write_output.jl
@@ -183,7 +183,7 @@ if nvpts==1
 else
 	for i=1:nin
 		for j=1:nout
-			str*="set term x11 persist $i\nset logscale x\nset xlabel 'Frequency [Hz]'\nset ylabel 'vpoint'\nset zlabel 'Gain [dB]' rotate by 90\nsplot 'bode.out' using 1:2:2+$(i*nout+j) with lines title item_out($j).'/'.item_in($i)\n"
+			str*="set term x11 persist $((i-1)*nout+j)\nset logscale x\nset xlabel 'Frequency [Hz]'\nset ylabel 'vpoint'\nset zlabel 'Gain [dB]' rotate by 90\nsplot 'bode.out' using 1:2:$(2+(i-1)*nout+j) with lines title item_out($j).'/'.item_in($i)\n"
 		end
 	end
 	str*="unset logscale x\nset yrange [-60<*:]\nset xzeroaxis\nset xlabel 'vpoint'\nset ylabel 'Eigenvalue [rad/s]'\nset term x11 persist $(nin*nout+1)\nplot 'eigen.out' using 2:(abs(\$3)<1e-4?NaN:\$3) with points pt 7 lw 2 title 'Real', 'eigen.out' using 2:(abs(\$4)<1e-4?NaN:\$4) with points pt 6 lw 2 title 'Imaginary'"
