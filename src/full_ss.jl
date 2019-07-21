91e8c32f6a31997cdbc71d157b1b6673e5996bce
BPMinaker <bminaker@uwindsor.ca>
2019-07-21 01:02:50 -0400

    bug fix

--- src/full_ss.jl
+++ src/full_ss.jl
@@ -21,7 +21,7 @@ for i=1:nvpts
 	m=size(dss_eqns[i].phys,1)
 	n=size(dss_eqns[i].B,2)
 
-	temp=dss_data(dss_eqns[i].A,dss_eqns[i].B,dss_eqns[i].phys,zeros(m,n),dss_eqns[i].E)
+	temp=dss_data(dss_eqns[i].A,dss_eqns[i].B,dss_eqns[i].phys,zeros(m,n),dss_eqns[i].E,dss_eqns[i].phys)
 	result[i]=dss2ss(temp,verbose && i<2)  ## Reduce to standard form
 end
 
