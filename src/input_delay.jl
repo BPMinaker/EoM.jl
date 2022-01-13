30e87898f0fc351810560f91f08332cedd919a92
BPMinaker <bminaker@gmail.com>
2022-01-13 09:15:34 -0500

    fix for input_delay

--- src/input_delay.jl
+++ src/input_delay.jl
@@ -3,16 +3,16 @@ function input_delay!(system::EoM.mbd_system, result::EoM.analysis, delay::Float
     nin = size(result.freq_resp[1],2)
 
     n = length(result.w)
+    T = zeros(nin, nin) * 1im + I
+    c = deleteat!(collect(1:nin), inp[2])
+    T = T[:, c]
+
     for i in 1:n
-        T = zeros(nin, 1) * 1im
-        T[inp[1], 1] = 1.0
-        T[inp[2], 1] = exp(-result.w[i] * 1im * delay)
+        T[inp[2], inp[1]] = exp(-result.w[i] * 1im * delay)
         result.freq_resp[i] *= T
     end
 
-    T = zeros(nin, 1)
-    T[inp[1], 1] = 1.0
-    T[inp[2], 1] = 1.0
+    T[inp[2], inp[1]] = 1.0
     result.ss_resp *= T
 
     mag(x) =  20 * log10.(abs.(x)) .+ eps(1.0)
