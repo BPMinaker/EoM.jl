f6b1a047846264439afb02c8fe25a761214eb830
BPMinaker <bminaker@uwindsor.ca>
2022-05-25 15:59:55 -0400

    bode plot limits

--- src/analyze.jl
+++ src/analyze.jl
@@ -109,7 +109,7 @@ function analyze(dss_eqns::EoM.dss_data, verb::Bool = false)
     end
 
     t = unique(abs.(result.e_val))
-    t = t[t .> 4π / 10]
+    t = t[t .> 4π / 100]
     t = t[t .< π * 1000]
     low = floor(log10(0.5 * minimum(t) / 2π))
     # lowest low eigenvalue, round number in Hz
