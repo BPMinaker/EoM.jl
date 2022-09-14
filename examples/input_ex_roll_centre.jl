5a3a1a1a55bdc63d9987af1443e86cd376a47346
Bruce Minaker <bminaker@uwindsor.ca>
2022-09-14 16:09:41 -0400

    default tire cornering stiffness non-zero

--- examples/input_ex_roll_centre.jl
+++ examples/input_ex_roll_centre.jl
@@ -1,4 +1,4 @@
-function input_full_car_rc(; u=0, a=1.189, b=2.885 - 1.189, tf=1.595, tr=1.631, kf=17000, kr=19000, cf=1000, cr=1200, m=16975 / 9.81, Ix=818, Iy=3267, Iz=3508, kt=180000, muf=35, mur=30, hf=0.1, hr=0.2, hG=0.4, krf=100, krr=100, cfy=0, cry=0, r = 0.3)
+function input_full_car_rc(; u=0, a=1.189, b=2.885 - 1.189, tf=1.595, tr=1.631, kf=17000, kr=19000, cf=1000, cr=1200, m=16975 / 9.81, Ix=818, Iy=3267, Iz=3508, kt=180000, muf=35, mur=30, hf=0.1, hr=0.2, hG=0.4, krf=100, krr=100, cfy=40000, cry=40000, r = 0.3)
 
     ## Copyright (C) 2017, Bruce Minaker
     ## input_ex_full_car.jl is free software; you can redistribute it and/or modify it
