bf0ea4673e33f74eb15ead6bb578d3ad74fcab0c
Bruce Minaker <bminaker@uwindsor.ca>
2025-06-08 23:05:53 -0400

    fix plot

--- src/ltisim.jl
+++ src/ltisim.jl
@@ -80,9 +80,9 @@ function ltiplot(
 
     if scale == 0
         scale = Int(round(length(t)/2000))
-        scale < 1 && (scale = 1)
     end
-  
+    scale < 1 && (scale = 1)
+    
     plot(t[1:scale:end], [hcat(obj.y.(t[1:scale:end])...)'[:, yidx] hcat(obj.u.(t[1:scale:end])...)'[:, uidx] data[1:scale:end,:]]; xlabel, ylabel, label, title, titlefontsize, titlelocation, lw, size, kwargs...)
 end
 
